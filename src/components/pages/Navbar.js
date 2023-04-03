ff4826b6228fe507fcc70a70efe469e44d4f3c80
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-04-03 01:22:36 -0700

    adding navbar

---
+++ src/components/pages/Navbar.js
@@ -0,0 +1,49 @@
+import { Navbar } from "react-bootstrap";
+
+function Nabbar() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <div className="navbar-brand">Mohamed Farah</div>
+        <hr />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto ms-auto nav-underline">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="aboutme.html">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="projects.html">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="message.html">
+                Contact Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="resume.html">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
