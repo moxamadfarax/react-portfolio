a8ccd3636ee04478857e5d51b9d6e6c397953959
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-04-03 04:50:36 -0700

    finished text area render and styles

--- src/components/pages/message.js
+++ src/components/pages/message.js
@@ -1,3 +1,48 @@
-function Message() {}
+function Message() {
+  const styles = {
+    container: { paddingbottom: "370px" },
+    textArea: { height: "20em" },
+  };
+
+  return (
+    <main class="container" style={styles.container}>
+      <form>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder="Enter your name"
+            name="name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Enter your email"
+            name="email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea
+            style={styles.textArea}
+            class="form-control"
+            id="message"
+            placeholder="Enter your message"
+            name="message"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
 
 export default Message;
